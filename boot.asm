69c40a27ff9ee69c6a9af254548c7afbe088329a
hailin.cai <hailin.cai.1975@gmail.com>
2026-01-01 23:34:55 -0500

    Day03 create asm version print_string

--- boot.asm
+++ boot.asm
@@ -1,19 +1,36 @@
-[org 0x7c00]          ; 告訴編譯器，這段代碼預計會被載入到記憶體地址 0x7c00
+[org 0x7c00]
 
-mov ah, 0x0e          ; BIOS 中斷服務 10h：顯示字元 (Teletype mode)
-mov al, 'H'           ; 將字元 'H' 存入 al 寄存器
-int 0x10              ; 呼叫中斷，由 BIOS 負責把字元印在螢幕上
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+; --- 主程式區段 ---
+mov si, HELLO_MSG    ; 將字串的首位址交給 SI
+call print_string    ; 呼叫函式
 
-jmp $                 ; $ 表示當前地址，這行會讓 CPU 在這裡無限循環，防止跑飛
+mov si, BYE_MSG      ; 切換到另一個字串
+call print_string
 
-; 填充與簽名
-times 510-($-$$) db 0 ; 用 0 填充剩餘空間，確保檔案總大小剛好為 510 字節
-dw 0xaa55             ; 最後 2 字節：寫入啟動標誌 (0x55, 0xAA)
+jmp $                ; 無限循環，卡住 CPU
+
+; --- 函式：print_string ---
+; 目的：印出以 0 結尾的字串
+print_string:
+    pusha            ; 保護暫存器現場
+.loop:
+    lodsb            ; 從 SI 讀取到 AL，SI 自動遞增
+    cmp al, 0        ; 判斷是否為字串結尾 (Null Terminator)
+    je .done         ; 如果是 0，跳轉到結束標籤
+    
+    mov ah, 0x0e     ; BIOS 電傳打字模式
+    int 0x10         ; 呼叫中斷顯示字元
+    jmp .loop        ; 繼續循環處理下一個字元
+.done:
+    popa             ; 還原暫存器現場
+    ret              ; 返回主程式
+
+; --- 資料區段 ---
+HELLO_MSG:
+    db 'Hello, OS World!', 0x0d, 0x0a, 0  ; 0x0d, 0x0a 是換行與回車
+BYE_MSG:
+    db 'Successfully printed string!', 0x0d, 0x0a, 0
+
+; --- 引導扇區填充分區 ---
+times 510-($-$$) db 0
+dw 0xaa55
